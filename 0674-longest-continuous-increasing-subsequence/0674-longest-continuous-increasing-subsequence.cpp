b0c87d5fccd054e9c9f2ed9e30a615e8e7b82edf
SHYAMA MISHRA <shyamamishra768@gmail.com>
2026-07-06 21:38:39 +0530

    Time: 0 ms (100%), Space: 14.8 MB (64.61%) - LeetHub

---
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+
+        int count = 1;
+        int maxCount = 1;
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] > nums[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+            }
+
+            maxCount = max(maxCount, count);
+        }
+
+        return maxCount;
+    }
+};
